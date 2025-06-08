913ce9eb82708f4e858ffd692ebe3325fb3af4f9
gabrielfst30 <gabrielfst30@gmail.com>
2025-06-08 13:53:56 -0300

    feature: definindo parametro para função gerenate e importando env

--- gemini_chat.py
+++ gemini_chat.py
@@ -1,23 +1,41 @@
 # To run this code you need to install the following dependencies:
 # pip install google-genai
 
-from dotenv import load_dotenv  # This is the new import you need
-
-load_dotenv()
-
+import base64
 import os
+from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+# Load environment variables from .env file
+load_dotenv()
 
-def generate():
+def generate(input):
     client = genai.Client(
-        api_key=os.environ.get("GEMINI_API_KEY"),
+        api_key=os.getenv("GEMINI_API_KEY"),
     )
-    # TAREFA 1: escolher o modelo apropriado e ajustar entrada do usuário e retorno da função
 
+    model = "gemini-2.0-flash"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text=input),
+            ],
+        ),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        response_mime_type="text/plain",
+    )
 
-#if __name__ == "__main__":
-#    # Teste para ter certeza que funciona
-#    test_response = generate(entrada="Olá, como está o tempo hoje em Cuiabá?")
-#    print(f"Test response: {test_response}")
+    response_text = ""
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        response_text += chunk.text
+    return response_text
+
+if __name__ == "__main__":
+    generate()
